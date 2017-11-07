19916c0a37c607ba40d32e638a811bf258c99796
lifeng.zqgame <280799743@qq.com>
2017-11-07 17:37:49 +0800

    no message

--- AwesomeProject/index.ios.js
+++ AwesomeProject/index.ios.js
@@ -12,41 +12,24 @@ import {
 } from 'react-native';
 
 
-
-
-class Blink extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { showText: true };
-
-        // 每1000毫秒对showText状态做一次取反操作
-        setInterval(() => {
-            this.setState(previousState => {
-                return { showText: !previousState.showText };
-            });
-        }, 1000);
-    }
-
+class  BlinkApp extends Component {
     render() {
-        // 根据当前showText的值决定是否显示text内容
-        let display = this.state.showText ? this.props.text : ' ';
-        return (
-            <Text>{display}</Text>
-        );
-    }
+        <View style={styles.container}>
+            {/*返回6个包*/}
+
+        </View>
+    },
+
+    //返回所有的包
+    
 }
 
-class BlinkApp extends Component {
-    render() {
-        return (
-            <View>
-                <Blink text='I love to blink' />
-                <Blink text='Yes blinking is so great' />
-                <Blink text='Why did they ever take this out of HTML' />
-                <Blink text='Look at me look at me look at me' />
-            </View>
-        );
+const styles = Stylesheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#F5FCF'
     }
-}
+});
+
 
 AppRegistry.registerComponent('AwesomeProject', () => BlinkApp);
--- BViewDemo/index.ios.js
+++ BViewDemo/index.ios.js
@@ -12,7 +12,9 @@ import {
   View
 } from 'react-native';
 
-export default class BViewDemo extends Component {
+/**-------------------第一个示例程序--------------------*/
+
+export  class BViewDemo extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -37,8 +39,9 @@ const styles = StyleSheet.create({
       //改变主轴方向----> 默认是竖向
       flexDirection:'row',
       //设置主轴的对齐方式
-      justifyContent:'center'
+      justifyContent:'center',
       //设定侧轴的对齐方式
+      alignItems: 'center'
   },
   innerViewStyle:{
       //上边距
@@ -51,7 +54,113 @@ const styles = StyleSheet.create({
     innerViewStyle2:{
         backgroundColor:  'yellow',
         width:100
+    },
+  container2: {
+      backgroundColor: 'purple',
+      marginTop:25,
+      flexDirection:'row',
+      justifyContent:'space-around',
+      alignItems:'center'
+
+  }
+});
+
+
+/**-------------------第二个示例程序--------------------*/
+
+
+export  class BViewDemo2 extends Component {
+    render() {
+        return (
+            <View style={styles.container2}>
+                <Text style={{backgroundColor:'red',height:30}}>第一个</Text>
+                <Text style={{backgroundColor:'green',height:40}}>第二个</Text>
+                <Text style={{backgroundColor:'blue',height:50}}>第三个</Text>
+                <Text style={{backgroundColor:'yellow',height:60}}>第四个</Text>
+            </View>
+        );
+    }
+}
+
+/**-------------------第三个示例程序--------------------*/
+
+export  class BViewDemo3 extends Component {
+    render() {
+        return (
+            <View style={styles1.container}>
+                <Text style={{backgroundColor:'red',width:80}}>第一个</Text>
+                <Text style={{backgroundColor:'green',width:90}}>第二个</Text>
+                <Text style={{backgroundColor:'blue',width:100}}>第三个</Text>
+                <Text style={{backgroundColor:'yellow',width:110}}>第四个</Text>
+            </View>
+        );
+    }
+}
+
+const  styles1 = StyleSheet.create({
+    container:{
+        backgroundColor: 'purple',
+        marginTop:25,
+        flexDirection:'row',
+        justifyContent:'flex-start',
+        alignItems:'center',
+        //显示不下，换一行
+        flexWrap:'wrap'
     }
 });
 
-AppRegistry.registerComponent('BViewDemo', () => BViewDemo);
+/**-------------------第四个示例程序--------------------*/
+
+export  class BViewDemo4 extends Component {
+    render() {
+        return (
+            <View style={styles2.container}>
+                <Text style={{backgroundColor:'red',flex:1,height:60,alignSelf:'flex-start'}}>第一个</Text>
+                <Text style={{backgroundColor:'green',flex:1,height:70,alignSelf:'flex-end'}}>第二个</Text>
+                <Text style={{backgroundColor:'blue',flex:2,height:80}}>第三个</Text>
+                <Text style={{backgroundColor:'yellow',flex:3,height:90}}>第四个</Text>
+            </View>
+        );
+    }
+}
+
+const  styles2 = StyleSheet.create({
+
+    container:{
+        backgroundColor: 'purple',
+        marginTop:25,
+        flexDirection:'row',
+        justifyContent:'flex-start',
+        alignItems:'center',
+    }
+});
+
+/**-------------------第五个示例程序--------------------*/
+//引入类库(Dimensions，获取尺寸的相关类库)
+var  Dimensions = require('Dimensions');
+
+export  class BViewDemo5 extends Component {
+    render() {
+        return (
+            <View style={styles5.outViewSytle}>
+                <Text>当前屏幕的宽度:{Dimensions.get('window').width}</Text>
+                <Text>当前屏幕的高度:{Dimensions.get('window').height}</Text>
+                <Text>当前屏幕的分辨率:{Dimensions.get('window').scale}</Text>
+            </View>
+        );
+    }
+}
+
+const styles5 = StyleSheet.create({
+    outViewSytle:{
+        flex:1,
+        //主轴方向居中
+        justifyContent:'center',
+        //侧轴方向居中
+        alignItems:'center',
+        //背景
+        backgroundColor:'red'
+    }
+});
+
+AppRegistry.registerComponent('BViewDemo', () => BViewDemo5);
